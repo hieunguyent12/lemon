27779f61f76575d309223cbd2bfcbc48ad57ee4c
Hieu Nguyen <hieunguyen.0527@gmail.com>
2022-02-06 21:35:33 -0600

    aaa

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,6 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
-import { ClassListItem } from "../ui/ClassListItem";
 
 const Home: NextPage = () => {
-  return <ClassListItem isTeacher _className="Pre-cal Honors" period={7} />;
+  return <p>Hi</p>;
 };
 export default Home;
