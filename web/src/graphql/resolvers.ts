612a8711acd4d1b5fbaaeb7335921d6a0327a0e3
Hieu Nguyen <hieunguyen.0527@gmail.com>
2022-02-23 19:05:44 -0600

    prepare to test deployment

--- web/src/graphql/resolvers.ts
+++ web/src/graphql/resolvers.ts
@@ -1,5 +1,4 @@
 import { JWT } from "next-auth/jwt";
-import { prisma } from "../prisma";
 import { Resolvers } from "./generated";
 
 function parseClassResponse(_class: any) {
@@ -33,115 +32,18 @@ const resolvers: Resolvers<Context> = {
       };
     },
     async classes(_, args, context) {
-      const { user } = context;
-
-      if (user.role === "student") {
-        // check in enrollments
-        const enrollments = await prisma.enrollment.findMany({
-          where: {
-            studentID: user.sub,
-          },
-          include: {
-            class: true,
-          },
-        });
-        return enrollments.map((enrollment) => ({
-          enrollmentId: enrollment.id,
-          ...enrollment.class,
-        }));
-      } else {
-        // just check for classes
-        return await prisma.class.findMany({
-          where: {
-            teacherID: user.sub,
-          },
-        });
-      }
+      return null;
     },
     async class(_, args, context) {
-      const { user } = context;
-      const { id } = args;
-
-      if (user.role === "student" && user.sub) {
-        const enrollment = await prisma.enrollment.findUnique({
-          where: {
-            id,
-          },
-          include: {
-            class: {
-              include: {
-                classes_assignments: {
-                  include: {
-                    assignment: true,
-                  },
-                },
-              },
-            },
-          },
-        });
-
-        if (enrollment && enrollment.class) {
-          return parseClassResponse(enrollment.class);
-        } else {
-          return null;
-        }
-      }
-
-      if (user.role === "teacher" && user.sub) {
-        const _class = await prisma.class.findUnique({
-          where: {
-            id,
-          },
-          include: {
-            classes_assignments: {
-              include: {
-                assignment: true,
-              },
-            },
-          },
-        });
-
-        if (_class && _class.teacherID === user.sub) {
-          return parseClassResponse(_class);
-        } else {
-          return null;
-        }
-      }
-
       return null;
     },
   },
   Mutation: {
     async updateUser(_, args) {
-      const { userId, name, role } = args;
-
-      let isTeacher = role === "teacher";
-
-      return await prisma.user.update({
-        where: {
-          id: userId,
-        },
-        data: {
-          profileName: name,
-          isTeacher,
-          isNewUser: false,
-        },
-      });
+      return null;
     },
     async createClass(_, args, context) {
-      if (context.user.role === "student") return null;
-
-      if (!context.user.sub) return null;
-
-      const { name, subject = null } = args;
-
-      return await prisma.class.create({
-        data: {
-          name,
-          subject,
-          teacherID: context.user.sub,
-        },
-      });
+      return null;
     },
   },
 };
